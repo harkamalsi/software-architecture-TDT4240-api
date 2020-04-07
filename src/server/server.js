aeb83985476f843071f29a54725efd5319d722ba
harkamalsi <harkamal_guraya@hotmail.com>
2020-04-07 14:26:37 +0200

    Added lobby state backend.

--- src/server/server.js
+++ src/server/server.js
@@ -4,8 +4,8 @@ const socketio = require('socket.io');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-/* const Constants = require('../shared/constants');
-const Game = require('./game'); */
+const Game = require('./components/Game');
+const Constants = require('./constants');
 
 // Environment variables
 require('dotenv').config();
@@ -28,7 +28,7 @@ mongoose.connect(uri, {
   useCreateIndex: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  dbName: 'inverse-pacman'
+  dbName: 'inverse-pacman',
 });
 
 const connection = mongoose.connection;
@@ -44,23 +44,198 @@ app.use('/game', gameRouter);
 // Setup socket.io
 const io = socketio(server);
 
-let interval;
+// array of objects (dicts) for lobbies and clients inside them with their socketids and playerids
+// each key is a new lobby&clients item
+let lobby_counter = 1;
+let lobbies = [];
 
-// Listen for socket.io connections
-io.on('connection', socket => {
+let total_clients = 0;
+
+let room;
+let clientId;
+let socketId;
+
+// Listen for socket.io connections for
+io.on('connection', (socket) => {
+  // socketId = socket.id;
   console.log('Player connected!', socket.id);
 
-  if (interval) clearInterval(interval);
+  socket.emit('hello', `main lobby with socketID ${socket.id}`);
 
-  socket.emit('players', `${socket.id}`);
+  socket.on(Constants.MSG_TYPES.JOIN_LOBBY, (data) => {
+    const { socketid, room, nickname, playerId } = data;
+    clientId = playerId;
+    socketId = socketid;
 
-  /* interval = setInterval(() => {
-    console.log('Player is still connected..');
-    //socket.emit('players', `${socket.id} is still connected..`);
-  }, 20000); */
+    if (
+      getClientsInRoom('lobby1') < Constants.MAXIMUM_CLIENTS_ALLOWED_PER_LOBBY
+    ) {
+      socket.join(room, () => {
+        updateLobbies(room, socketId, clientId);
+      });
 
-  socket.on('disconnect', () => console.log('Player disconnected!'));
+      socket.emit('update', {
+        room,
+        message: `${room} with socketID ${socket.id}`,
+      });
+    } else {
+      console.log(`${room} is full!`);
+      io.to(socketId).emit('update', {
+        room,
+        message: '',
+        error: `${room} is full!`,
+      });
+    }
+  });
+
+  socket.on(Constants.MSG_TYPES.CREATE_LOBBY, (data) => {
+    const { socketid, nickname, playerId } = data;
+    clientId = playerId;
+    socketId = socketid;
+
+    room = `lobby${lobby_counter}`;
+
+    socket.join(room, () => {
+      updateLobbies(room, socketId, playerId);
+      lobby_counter++;
+    });
+
+    socket.emit('update', {
+      room,
+      message: `${room} with socketID ${socketId}`,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    // Finding the room player disconnected from
+    if (lobbies.length > 0) {
+      lobbyItem = lobbies.find((lobbyItem) =>
+        lobbyItem.sockets.includes(socketId)
+      );
+
+      if (lobbyItem) room = lobbyItem.room;
+      console.log('------------', room, socketId, clientId);
+    }
+
+    updateLobbies(room, socketId, clientId, true);
+
+    //console.log(lobbies);
+  });
 });
+
+// Setup the Gamed
+const game = new Game();
+
+const updateLobbies = (
+  room,
+  socketId,
+  clientId,
+  playerDisconnected = false
+) => {
+  if (playerDisconnected || room == undefined) {
+    roomClientsNumber = getClientsInRoom(room);
+
+    if (roomClientsNumber == 0 && lobby_counter >= 1) {
+      // finds if lobby exists before
+      const lobbyIndex = lobbies.findIndex(
+        (lobbyItem) => lobbyItem.room == room
+      );
+
+      if (lobbyIndex > -1) {
+        lobbies.splice(lobbyIndex, 1);
+        lobby_counter--;
+      }
+    } else {
+      // roomClientsNumber > 0
+      const lobbyIndex = lobbies.findIndex(
+        (lobbyItem) => lobbyItem.room == room
+      );
+
+      if (lobbyIndex > -1) {
+        lobbyItem = lobbies[lobbyIndex];
+
+        newSockets = lobbyItem.sockets.filter(
+          (socketItem) => socketItem != socketId
+        );
+        newPlayerIds = lobbyItem.playerIds.filter(
+          (playerIdItem) => playerIdItem != clientId
+        );
+
+        let newLobbyItem = {
+          room,
+          sockets: newSockets,
+          playerIds: newPlayerIds,
+        };
+
+        lobbies[lobbyIndex] = newLobbyItem;
+
+        // lobbyItem.sockets.splice(socketIndex, 1);
+        // lobbyItem.playerIds.splice(playerIndex, 1);
+
+        if (getClientsInRoom(room) == 0 && lobby_counter >= 1) {
+          // TODO: rightly decrement lobby_counter
+          // lobby_counter--; Not decremening lobby_counter.
+          // For now only for last lobby deleted => decrement lobby_counter
+          console.log(
+            'lobby_counter',
+            lobby_counter,
+            'not correctly decremented.'
+          );
+        }
+      }
+    }
+  } else {
+    // finds if lobby exists before
+    const lobbyIndex = lobbies.findIndex((element) => element.room == room);
+
+    let socketAlreadyJoined = false;
+
+    // checks only if socket is in this lobby (clients only join one lobby at a time)
+    if (lobbyIndex != -1) {
+      socketAlreadyJoined = lobbies[lobbyIndex].sockets.includes(socketId);
+    }
+
+    //lobbies.findIndex(element =>
+    //  element.sockets.includes(socketID)
+
+    if (lobbyIndex != -1 && !socketAlreadyJoined) {
+      lobbyItem = lobbies[lobbyIndex];
+
+      let newLobbyItem = {
+        room,
+        sockets: [...lobbyItem.sockets, socketId],
+        playerIds: [...lobbyItem.playerIds, clientId],
+      };
+
+      lobbies[lobbyIndex] = newLobbyItem;
+    } else if (lobbyIndex == -1) {
+      lobbies.push({
+        room,
+        sockets: [socketId],
+        playerIds: [clientId],
+      });
+    }
+  }
+
+  //console.log(lobbies);
+};
+
+const getTotalClients = () => {
+  total_clients = Object.keys(io.of('/').sockets).length;
+
+  lobby1_clients = getClientsInRoom('lobby1');
+
+  return { total_clients, lobby1_clients };
+};
+
+const getClientsInRoom = (room) => {
+  const clients = io.sockets.adapter.rooms[room];
+
+  if (clients) {
+    return clients.length;
+  }
+  return 0;
+};
 
 // Starts the server
 server.listen(PORT, () => {
--- src/socket-io-client/src/App.js
+++ src/socket-io-client/src/App.js
@@ -6,46 +6,106 @@ class App extends Component {
     super();
     this.state = {
       response: false,
+      response1: false,
       endpoint: 'http://localhost:5000',
       playerId: '5e60f7f7971e074aa0abddc0',
-      player: false
+      nickname: false,
+      socket: null,
+      lobbyname: 'lobby1',
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-    socket.on('players', data => this.setState({ response: data }));
+    this.setState({ socket });
 
-    this.fetchPlayer();
+    socket.on('hello', (data) => {
+      this.setState({ response: data });
+    });
+
+    // Emitting playerId to server
+    socket.emit('playerId', this.state.playerId);
+
+    await this.fetchPlayer();
   }
 
-  fetchPlayer() {
-    fetch(this.state.endpoint + '/api/players/id/' + this.state.playerId)
-      .then(response => {
+  async fetchPlayer() {
+    await fetch(this.state.endpoint + '/api/players/id/' + this.state.playerId)
+      .then((response) => {
         return response.json();
       })
-      .then(player => {
-        this.setState({ player });
+      .then((user) => {
+        return user.nickname;
+      })
+      .then((nickname) => {
+        this.setState({ nickname });
+        return nickname;
       });
   }
 
-  render() {
-    const { response, player } = this.state;
+  joinLobby(room) {
+    const { socket, nickname, playerId } = this.state;
+    //const room = 'lobby1';
 
-    let nickname;
-    if (player) nickname = player.nickname;
+    const payload = { socketid: socket.id, room, nickname, playerId };
+
+    // Emitting nickname to server side for joining a lobby with name
+    socket.emit('join_lobby', payload);
+
+    // Emitting nickname to server side for creating a lobby; not sending a name
+    //console.log('Creating a lobby without providing a lobby name');
+    //socket.emit('create_lobby', { socketid: socket.id, nickname, playerId });
+
+    this.fetchUpdates('update');
+  }
+
+  fetchUpdates(event) {
+    // Now this socket is connected to lobby1 and will on default listen here
+    this.state.socket.on(event, (data) => {
+      console.log(data);
+      this.setState({ response1: data });
+    });
+  }
+
+  render() {
+    const { response, response1, nickname } = this.state;
+
+    // this.joinLobby(this.state.socket);
 
     return (
       <div style={{ textAlign: 'center' }}>
         {response ? (
           <div>
-            <p>Player connected is: {response}</p>
+            <p>Player connected to: {response}</p>
             <p>Player nickname is: {nickname}</p>
           </div>
         ) : (
-          <p>Loading...</p>
+          <p>Main lobby loading...</p>
         )}
+        <br />
+        <p>
+          ----------------------------------------------------------------------------
+        </p>
+        <br />
+        <br />
+        {response1 ? (
+          response1.error ? (
+            <div>
+              <p>Error is: {response1.error}</p>
+            </div>
+          ) : (
+            <div>
+              <p>Player connected to: {response1.room}</p>
+              <p>Player nickname is: {nickname}</p>
+            </div>
+          )
+        ) : (
+          <p>Lobby1 loading...</p>
+        )}
+        <br />
+        <br />
+        <button onClick={() => this.joinLobby('lobby1')}>Join lobby1</button>
       </div>
     );
   }
